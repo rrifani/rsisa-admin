89c11c847d92b41338ab37d6f74e2fee36d42e51
rrifani <rezarifani26@gmail.com>
2026-08-17 15:54:52 +0800

    feat: :sparkles: enhance AuthService with necessary imports and DTOs

--- apps/api/src/app/auth/auth.service.ts
+++ apps/api/src/app/auth/auth.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtHelper } from '@rsisa-admin/shared';
+import { SUPER_ADMIN_GROUP_ID } from '../../constants';
+import {
+  CredentialDto,
+  LoginResultDto,
+  UserIdentityDto,
+  UserAccountRow,
+} from './auth.dto';
+import { UserRepository } from './auth.repository';
 
 @Injectable()
 export class AuthService {}
